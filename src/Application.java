13e11cc6af937c128e6441e5a43bfd9e6293c03a
Oleg Zherebtsov <o.zherebtsov93@yandex.ru>
2024-11-02 22:21:28 +0300

    v.0

--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Application {
+    public static void main(String[] args) {
+
+        System.out.println("Здравствуйте! Я программа Шифровальшик.");
+        System.out.println("Я умею выполнять следующие функции:");
+        System.out.println("1. Шифровать файл с помощью ключа.");
+        System.out.println("2. Дешифровать файл с помощью ключа.");
+        System.out.println("3. Дешифровать методом перебора");
+        System.out.println("4. Выход");
+        System.out.println("Введите номер функции которую хотите, чтобы я выполнил.");
+
+        Scanner console = new Scanner(System.in);
+
+        switch (console.nextLine()) {
+            case "1":
+                System.out.println("Укажите путь к файлу который необходимо зашифровать.");
+                String str = console.nextLine();
+
+                System.out.println("Укажите ключ шифрования.");
+                int key = console.nextInt();
+
+                System.out.println("Укажите путь куда сохранить зашифрованный файл.");
+                String strFinal = console.nextLine();
+
+
+        }
+
+
+    }
 }
--- src/Cipher.java
+++ src/Cipher.java
@@ -1,3 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
 public class Cipher {
 
+    private static final List ALPHABET = Arrays.asList('а', 'б',
+            'в','г', 'д', 'е', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+            'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»',
+            ':', '!', '?', ' ');
+
+    public Files Encrypt(String str, int key, String strFinal) {
+
+        String string = "";
+        char c;
+        char a;
+
+        try {
+            FileReader fr = new FileReader(str);
+            BufferedReader br = new BufferedReader(fr);
+            FileWriter fw = new FileWriter(strFinal);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            while ((string = br.readLine()) != null){
+                c = (char) br.read();
+                if (ALPHABET.contains(c)) {
+                    int i = (ALPHABET.indexOf(c) + key) % ALPHABET.size();
+                    a = (char) ALPHABET.get(i);
+                }
+            }
+
+
+
+        } catch (Exception e) {
+
+        }
+
+
+    }
 }
